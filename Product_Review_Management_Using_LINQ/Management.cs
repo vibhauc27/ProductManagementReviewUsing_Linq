926156cb1b7a5fb1102cca53a13c0d06f8c688c9
Vibhauc_27 <ucvibha@gmail.com>
2022-04-01 16:00:16 +0530

    [Vibha]Added:To retreive top 3 records from the list who's rating is high using LINQ

---
+++ Product_Review_Management_Using_LINQ/Management.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Review_Management_Using_LINQ
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview   //LINQ querry
+                                orderby productReviews.Rating descending  //these 2 lines retreive the records which are having highest rating
+                                select productReviews).Take(3);  //gives highest top 3 records
+
+            foreach (var list in recordedData)  //To check whether I have retreived proper records or not
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
+            }
+        }
+    }
+}
--- Product_Review_Management_Using_LINQ/Program.cs
+++ Product_Review_Management_Using_LINQ/Program.cs
@@ -6,7 +6,7 @@ namespace Product_Review_Management_Using_LINQ
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Product Review Management Problems Using LINQ");
+            Console.WriteLine("Welcome to Product Review Management Problem Using LINQ");
 
             List<ProductReview> productReviewList = new List<ProductReview>()
             {
@@ -14,7 +14,7 @@ namespace Product_Review_Management_Using_LINQ
             new ProductReview() { ProductId = 2, UserId = 1, Rating = 4, Review = "Good", isLike=true },
             new ProductReview() { ProductId = 3, UserId = 1, Rating = 7, Review = "Good", isLike=true },
             new ProductReview() { ProductId = 4, UserId = 1, Rating = 8, Review = "Good", isLike=true },
-            new ProductReview() { ProductId = 5, UserId = 1, Rating = 8, Review = "Nice", isLike=true },
+            new ProductReview() { ProductId = 5, UserId = 1, Rating = 10, Review = "Nice", isLike=true },
             new ProductReview() { ProductId = 6, UserId = 1, Rating = 9, Review = "Nice", isLike=true },
             new ProductReview() { ProductId = 7, UserId = 1, Rating = 6, Review = "Good", isLike=true },
             new ProductReview() { ProductId = 8, UserId = 1, Rating = 7, Review = "Good", isLike=false},
@@ -27,11 +27,16 @@ namespace Product_Review_Management_Using_LINQ
             new ProductReview() { ProductId = 15, UserId = 1, Rating = 5, Review = "Good", isLike=true },
             new ProductReview() { ProductId = 16, UserId = 1, Rating = 5, Review = "Good", isLike=true }
             };
-            Console.WriteLine("Rows in the table = ");
+
+           /* Console.WriteLine("Rows in the table = ");
             foreach (var list in productReviewList)
             {
                 Console.WriteLine("Product id = " + list.ProductId + "User id = " + list.UserId + "Rating is = " + list.Rating + " Review is = " + list.Review + " isLike = " + list.isLike);
-            }
+            }*/
+
+
+            Management management = new Management();
+            management.TopRecords(productReviewList);
         }
     }
 }
